5df6b5ccb2fbade86929b869406a7a5561bf22fe
Luciano Junior <engluciano.junior@gmail.com>
2021-09-23 04:52:24 -0300

    Update

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,8 @@
 const express = require('express');
-
 const routes = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Ola Mundo');
-})
-
+routes.get('/', (req, res) => {
+    res.send('Ola Mundo')
+});
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const routes = require('./routes');
-
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-//require('database');
 
 const app = express();
 
+app.use(routes);
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-app.use(routes());
+
 
 app.listen(3000);
-
-
-app.listen(3000, function () {
-    console.log("Server is running");
-});
+    console.log('SERVDOR RODANDO NA PORTA 3000')
