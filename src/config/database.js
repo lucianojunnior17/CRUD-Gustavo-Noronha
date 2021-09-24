de0a7ac3a587df877654a615842dc64fcdefec50
Luciano Junior <engluciano.junior@gmail.com>
2021-09-24 02:24:37 -0300

    Update

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,9 @@ module.exports = {
     dialect: 'mysql',
     host: 'localhost',
     username: 'root',
-    port: 3000,
-    password: '',
-    database: 'curso',
+    port: 3307,
+    password: '123456',
+    database: 'Curso CRUD',
     defines: {
         timestamps: true,
         underscores: true
